c93dd5d44d7a007e572f0a143ea4c3c1248fbc6c
Thomas Hodges <thodges@javascriptonline.org>
2019-08-08 16:21:06 -0700

    firstbit rendering components

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import HomeScreen from './src/screens/HomeScreen';
+import ComponentScreen from'./src/screens/ComponentScreen'
 
 const navigator = createStackNavigator(
   {
-    Home: HomeScreen
+    Home: HomeScreen,
+    Components: ComponentScreen
   },
   {
-    initialRouteName: 'Home',
+    initialRouteName: 'Components',
     defaultNavigationOptions: {
       title: 'App'
     }
