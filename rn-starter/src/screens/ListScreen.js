3937d6731219c794efb0bf635fb8621dd36fe33e
Thomas Hodges <thomas.hodges@clearcapital.com>
2019-08-09 12:26:17 -0700

    problem solution and margins reduced

--- rn-starter/src/screens/ListScreen.js
+++ rn-starter/src/screens/ListScreen.js
@@ -3,30 +3,28 @@ import {FlatList, StyleSheet, Text, View} from 'react-native'
 
 const ListScreen = () => {
   const friends = [
-    { name: 'Friend #1' },
-    { name: 'Friend #2' },
-    { name: 'Friend #3' },
-    { name: 'Friend #4' },
-    { name: 'Friend #5' },
-    { name: 'Friend #6' },
-    { name: 'Friend #7' },
-    { name: 'Friend #8' },
-    { name: 'Friend #9' }
+    { name: 'Friend #1', age: 76 },
+    { name: 'Friend #2', age: 94 },
+    { name: 'Friend #3', age: 67 },
+    { name: 'Friend #4', age: 86 },
+    { name: 'Friend #5', age: 36 },
+    { name: 'Friend #6', age: 54 },
+    { name: 'Friend #7', age: 59 },
+    { name: 'Friend #8', age: 63 },
+    { name: 'Friend #9', age: 90 }
   ]
   return (
     <FlatList 
-      horizontal
-      showsHorizontalScrollIndicator = {false}
       data = {friends}
       keyExtractor = {friend => friend.name}
-      renderItem = { ({item}) => <Text style={styles.textStyle}>{item.name}</Text>}
+      renderItem = { ({item}) => <Text style={styles.textStyle}>{item.name} - Age {item.age}</Text>}
     />
   )
 }
 
 const styles = StyleSheet.create({
   textStyle: {
-    marginVertical: 50
+    marginVertical: 20
   }
 })
 
