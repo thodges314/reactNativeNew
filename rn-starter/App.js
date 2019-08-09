ad1bde21c273df6307342f10b8bb5a296025dcf0
Thomas Hodges <thomas.hodges@clearcapital.com>
2019-08-09 12:12:51 -0700

    list no keys

--- rn-starter/App.js
+++ rn-starter/App.js
@@ -1,14 +1,16 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import HomeScreen from './src/screens/HomeScreen';
 import ComponentScreen from'./src/screens/ComponentScreen'
+import ListScreen from './src/screens/ListScreen'
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
-    Components: ComponentScreen
+    Components: ComponentScreen,
+    List: ListScreen
   },
   {
-    initialRouteName: 'Components',
+    initialRouteName: 'List',
     defaultNavigationOptions: {
       title: 'App'
     }
