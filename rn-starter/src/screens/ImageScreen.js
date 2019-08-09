5f1bbc62723bb3cd4c530033200d9299131068a4
Thomas Hodges <thomas.hodges@clearcapital.com>
2019-08-09 13:04:43 -0700

    ImageScreen start

---
+++ rn-starter/src/screens/ImageScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const ImageScreen = () => {
+  return ()
+}
+
+const styles = StyleSheet.create({})
+
+export default ImageScreen
