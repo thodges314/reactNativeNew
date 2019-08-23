3c14394119178ae5a970acd36f647257db11c4c6
Thomas Hodges <thodges@javascriptonline.org>
2019-08-22 18:51:32 -0700

    component built

---
+++ food/src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {StyleSheet, Text, View} from 'react-native'
+
+const SearchBar = () => {
+	return (
+		<View>
+			<Text>SearchBar</Text>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({})
+
+export default SearchBar
